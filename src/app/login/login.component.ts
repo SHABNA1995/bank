b71a59adcbf268d5d11b772613f21b340ad555b3
unknown <Shimnasshabna@gmail.com>
2023-07-11 10:54:03 +0530

    first committ

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  data="Happy Banking With Us"
+  pdata="Enter Account Number"
+
+  Login(){
+    alert("login clicked")}
+}
+
+
+
